8d53befc555a42bbbb5c634af353816eb2240f98
jonghwann <jonghwan0729@gmail.com>
2025-09-30 13:12:10 +0900

    [level 1] Title: 과일 장수, Time: 224.81 ms, Memory: 90.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,11 @@
+function solution(k, m, score) {
+    let result = 0;
+    
+    const sortedScore = score.slice().sort((a, b) => a - b).slice(score.length % m);
+    
+    for (let i = 0; i < sortedScore.length; i += m) {
+        result += sortedScore[i] * m;
+    }
+    
+    return result;
+}
