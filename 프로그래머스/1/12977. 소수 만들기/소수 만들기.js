f57a4eaf05ae427144a8bc341642dab469feff6e
jonghwann <jonghwan0729@gmail.com>
2025-10-01 13:06:29 +0900

    [level 1] Title: 소수 만들기, Time: 20.19 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,25 @@
+function primecheck(n) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    
+    return true;    
+}
+
+function solution(nums) {
+    let cnt = 0;
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (var k = j + 1; k < nums.length; k++) {
+                if (primecheck(nums[i] + nums[j] + nums[k])) {
+                    cnt++;
+                }
+            }
+        }
+    }
+    
+    return cnt;
+}
